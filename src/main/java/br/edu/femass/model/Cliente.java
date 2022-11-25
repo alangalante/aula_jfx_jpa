d3b81c46a0f967b2f1ade480dda9050747d3fb1a
Alan Carvalho Galante <alancarvalho@gmail.com>
2022-11-25 11:22:05 -0300

    Trabalhando com herança

--- src/main/java/br/edu/femass/model/Cliente.java
+++ src/main/java/br/edu/femass/model/Cliente.java
@@ -5,19 +5,12 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 
 @Entity
-public class Cliente {
+public class Cliente extends Pessoa{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nome;
     private String endereco;
 
     @OneToMany(cascade = CascadeType.ALL)
---
+++ src/main/java/br/edu/femass/model/Pessoa.java
@@ -0,0 +1,37 @@
+package br.edu.femass.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+    protected String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+}
